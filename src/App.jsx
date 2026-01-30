6bbdd134f0ba7c6cfb3335415426a8703a66c9e6
Taenggu99 <leetaeeun99@gmail.com>
2026-01-30 19:10:34 +0900

    feat : mypage 페이지 및 css 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import MainPage from './pages/Main/MainPage'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import MainPage from './pages/Main/MainPage';
+import MyPage from './pages/Profile/MyPage';
+import './App.css';
 
 function App() {
-  return <MainPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" replace />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/profile" element={<MyPage />} />
+        {/* TODO: 추가 페이지 라우트 */}
+        {/* <Route path="/feed" element={<FeedPage />} /> */}
+        {/* <Route path="/recipe-saved" element={<RecipeSavedPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
