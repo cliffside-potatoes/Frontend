7a5101f9250e3456df991e9f8f89f049161a96d4
Taenggu99 <leetaeeun99@gmail.com>
2026-01-31 22:19:26 +0900

    feat : signup 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './pages/Main/MainPage';
 import MyPage from './pages/Profile/MyPage';
+import SignUpPage from './pages/Auth/SignUpPage';
+import SignInPage from './pages/Auth/SignInPage';
 import './App.css';
 
 function App() {
@@ -11,9 +13,8 @@ function App() {
         <Route path="/" element={<Navigate to="/main" replace />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/profile" element={<MyPage />} />
-        {/* TODO: 추가 페이지 라우트 */}
-        {/* <Route path="/feed" element={<FeedPage />} /> */}
-        {/* <Route path="/recipe-saved" element={<RecipeSavedPage />} /> */}
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
