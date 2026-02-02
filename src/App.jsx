e43464a9343cb9db61b69dba5a98881ea7fcbb7c
sorrybro2 <mikely329@mgail.com>
2026-02-02 13:08:13 +0900

    feat(router): 라우트 설정 추가 - BrowserRouter, Routes로 /, /refrigerator, 카테고리 추가·설정 라우트 등록

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/Main/MainPage'
+import RefrigeratorPage from './pages/Refrigerator/RefrigeratorPage'
+import CategoryRegistrationPage from './pages/Refrigerator/CategoryRegistrationPage'
+import CategorySettingsPage from './pages/Refrigerator/CategorySettingsPage'
 import './App.css'
 
 function App() {
-  return <MainPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/refrigerator" element={<RefrigeratorPage />} />
+        <Route path="/refrigerator/category" element={<CategoryRegistrationPage />} />
+        <Route path="/refrigerator/category/settings" element={<CategorySettingsPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
