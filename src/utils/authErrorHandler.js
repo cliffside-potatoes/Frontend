a34e3f0f15eaea4d92a6f3050134e3bfb134609c
taenggu <leetaeeun99@gmail.com>
2026-04-01 13:47:32 +0900

    fix : 미인증시 로그아웃 하라는 팝업 제거

--- src/utils/authErrorHandler.js
+++ src/utils/authErrorHandler.js
@@ -1,15 +1,8 @@
-import {
-  clearStoredAuth,
-  getCurrentPath,
-  savePostLoginRedirect,
-} from './authStorage';
+import { invalidateAuthSession } from './authStorage';
 
 export const handleAuthError = (error) => {
   if (error?.status === 401) {
-    alert('로그인 정보가 만료되었습니다. 다시 로그인해주세요.');
-    savePostLoginRedirect(getCurrentPath());
-    clearStoredAuth();
-    window.location.href = '/signin';
+    invalidateAuthSession();
     return true;
   }
 
